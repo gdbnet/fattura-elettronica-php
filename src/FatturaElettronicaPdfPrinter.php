ac31e9b22d6e6a3ddd3579a56fc9d4cc591ace56
Giuseppe di Bona <giuseppe.dibona@performer.it>
2019-07-09 10:50:52 +0200

    resa statifa la funzione di pulizia per i p7m

---
+++ src/FatturaElettronicaPdfPrinter.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Gdbnet\FatturaElettronica;
+
+use Dompdf\Dompdf;
+
+/**
+ * Class FatturaElettronicaPdfPrinter
+ *
+ * @package Gdbnet\FatturaElettronica
+ */
+class FatturaElettronicaPdfPrinter implements FatturaElettronicaPrinterInterface
+{
+
+    /**
+     * @param string $xml
+     *
+     * @return mixed
+     */
+    public function stampa(string $xml)
+    {
+        $htmlPrinter = new FatturaElettronicaHtmlPrinter();
+        $html = $htmlPrinter->$this->stampa($xml);
+
+        $pdf = new Dompdf();
+
+        $pdf->loadHtml($html);
+
+        $pdf->setPaper('A4', 'porttrait');
+
+        $pdf->render();
+
+        $pdf->stream();
+
+        return true;
+    }
+}
